24f1c41c25210ffb9e943c4d372cf4f93df632bc
David Nacro <166960840+Davidncrdev@users.noreply.github.com>
2024-12-22 18:58:10 +0100

    primer commit

---
+++ src/utils/data.ts
@@ -0,0 +1,122 @@
+import visibilityDilemma from "../assets/images/visibility-dilemma.jpg";
+import engagementGap from "../assets/images/engagement-gap.jpg";
+import resultsInHibernation from "../assets/images/results-in-hibernation.jpg";
+
+const services = [
+  {
+    title: "Estrategias de Marketing Digital",
+    features: [
+      "Análisis de mercado",
+      "Ejecución multicanal",
+      "Optimización ROI",
+    ],
+    icon: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" stroke-width="1.5" fill="currentColor" class="w-6 h-6"><path d="M256 0c17.7 0 32 14.3 32 32V42.4c93.7 13.9 167.7 88 181.6 181.6H480c17.7 0 32 14.3 32 32s-14.3 32-32 32H469.6c-13.9 93.7-88 167.7-181.6 181.6V480c0 17.7-14.3 32-32 32s-32-14.3-32-32V469.6C130.3 455.7 56.3 381.7 42.4 288H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H42.4C56.3 130.3 130.3 56.3 224 42.4V32c0-17.7 14.3-32 32-32zM107.4 288c12.5 58.3 58.4 104.1 116.6 116.6V384c0-17.7 14.3-32 32-32s32 14.3 32 32v20.6c58.3-12.5 104.1-58.4 116.6-116.6H384c-17.7 0-32-14.3-32-32s14.3-32 32-32h20.6C392.1 165.7 346.3 119.9 288 107.4V128c0 17.7-14.3 32-32 32s-32-14.3-32-32V107.4C165.7 119.9 119.9 165.7 107.4 224H128c17.7 0 32 14.3 32 32s-14.3 32-32 32H107.4zM256 224a32 32 0 1 1 0 64 32 32 0 1 1 0-64z"/></svg>`,
+  },
+  {
+    title: "Diseño Web Personalizado",
+    features: [
+      "Diseños modernos y funcionales",
+      "Optimización para conversión",
+      "Integración de herramientas avanzadas",
+    ],
+    icon: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" stroke-width="1.5" fill="currentColor" class="w-6 h-6"><path d="M0 80v48c0 17.7 14.3 32 32 32H48 96V80c0-26.5-21.5-48-48-48S0 53.5 0 80zM112 32c10 13.4 16 30 16 48V384c0 35.3 28.7 64 64 64s64-28.7 64-64v-5.3c0-32.4 26.3-58.7 58.7-58.7H480V128c0-53-43-96-96-96H112zM464 480c61.9 0 112-50.1 112-112c0-8.8-7.2-16-16-16H314.7c-14.7 0-26.7 11.9-26.7 26.7V384c0 53-43 96-96 96H368h96z"/></svg>`,
+  },
+  {
+    title: "Gestión de Redes Sociales",
+    features: [
+      "Estrategia personalizada",
+      "Creación de contenido atractivo",
+      "Análisis y mejora constante",
+    ],
+    icon: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" stroke-width="1.5" fill="currentColor" class="w-6 h-6"><path d="M64 64c0-17.7-14.3-32-32-32S0 46.3 0 64V400c0 44.2 35.8 80 80 80H480c17.7 0 32-14.3 32-32s-14.3-32-32-32H80c-8.8 0-16-7.2-16-16V64zm406.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L320 210.7l-57.4-57.4c-12.5-12.5-32.8-12.5-45.3 0l-112 112c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L240 221.3l57.4 57.4c12.5 12.5 32.8 12.5 45.3 0l128-128z"/></svg>`,
+  }
+];
+
+const solutions = [
+  {
+    title: "Generación de contenido",
+    painPoint:
+      "llevamos años ayudando a autónomos y pequeñas empresas a destacar en el entorno digital. Nuestro enfoque se basa en entender las necesidades únicas de cada cliente, ofreciendo estrategias personalizadas que maximizan su impacto en redes sociales y presencia online.",
+    agitatepainPoint:
+      "",
+    solution:
+      "",
+    img: visibilityDilemma,
+  },
+  {
+    title: "Presencia local en Málaga",
+    painPoint:
+      "sede en Málaga, trabajamos mano a mano con nuestros clientes, brindando un trato cercano y soluciones adaptadas al mercado local. Entendemos los desafíos de los autónomos y PYMEs, y estamos aquí para ayudarlos a superar barreras y alcanzar el éxito..",
+      agitatepainPoint:
+      "",
+    solution:
+      "",
+    img: engagementGap,
+  },
+  {
+    title: "Innovación y tecnología",
+    painPoint:
+      "Nos especializamos en integrar herramientas avanzadas de inteligencia artificial con diseño web moderno y marketing digital. Cada proyecto que realizamos está diseñado para optimizar resultados, mejorar la experiencia del usuario y diferenciar a nuestros clientes en un mercado competitivo.",
+      agitatepainPoint:
+      "",
+    solution:
+      "",
+    img: resultsInHibernation,
+  },
+];
+
+const faqs = [
+  {
+    question: "¿Cómo puede el marketing en redes sociales beneficiar a mi negocio?",
+    answer:
+      "El marketing en redes sociales puede beneficiar significativamente a tu negocio al aumentar el reconocimiento de marca, involucrar a tu audiencia objetivo, dirigir tráfico a tu sitio web y generar clientes potenciales. Además, permite interactuar directamente con tus clientes, fomentando la lealtad a la marca y obteniendo información valiosa sobre el comportamiento del consumidor.",
+  },
+  {
+    question: "¿Qué diferencia a su agencia de las demás?",
+    answer:
+      "Nuestra agencia se destaca por una combinación de estrategias personalizadas, decisiones basadas en datos y un compromiso con la colaboración con nuestros clientes. Priorizamos entender los objetivos únicos de tu negocio y ofrecemos soluciones a medida que generan resultados medibles.",
+  },
+  {
+    question: "¿Cómo miden el éxito de las campañas en redes sociales?",
+    answer:
+      "Utilizamos un enfoque integral para medir el éxito de las campañas. Esto incluye el seguimiento de indicadores clave de rendimiento (KPI) como métricas de interacción, tasas de conversión, alcance y comentarios de los clientes. Nuestro objetivo es proporcionarte información transparente y accionable sobre la efectividad de tus esfuerzos en redes sociales.",
+  },
+  {
+    question: "¿Pueden dar ejemplos de campañas exitosas en redes sociales?",
+    answer:
+      "¡Por supuesto! Contamos con un portafolio de campañas exitosas en diversas industrias. Consulta nuestros Casos de Éxito para ver cómo hemos ayudado a negocios como el tuyo a alcanzar sus objetivos en redes sociales.",
+  },
+  {
+    question: "¿Ofrecen estrategias personalizadas para redes sociales?",
+    answer:
+      "Claro que sí. Nuestro equipo se especializa en crear estrategias personalizadas adaptadas a tu marca, industria y público objetivo. Creemos en el poder de la singularidad, y nuestras estrategias reflejan tus metas y aspiraciones específicas.",
+  },
+  {
+    question: "¿Cómo manejan los comentarios o reseñas negativas en redes sociales?",
+    answer:
+      "Tenemos un enfoque proactivo para gestionar la reputación en línea. Nuestras herramientas de escucha social nos permiten monitorear menciones en tiempo real. En caso de comentarios negativos, respondemos rápidamente con empatía y buscamos soluciones. Creemos en convertir los desafíos en oportunidades de mejora.",
+  },
+  {
+    question: "¿Con qué plataformas de redes sociales trabajan?",
+    answer:
+      "Trabajamos con una amplia gama de plataformas de redes sociales, incluyendo, entre otras, Facebook, Instagram, Twitter, LinkedIn, Pinterest y TikTok. Nuestras estrategias se adaptan a las plataformas más relevantes para tu público objetivo.",
+  },
+  {
+    question: "¿Con qué frecuencia recibiré informes sobre el rendimiento de mis campañas?",
+    answer:
+      "Proporcionamos informes regulares y detallados sobre el rendimiento de tus campañas en redes sociales. La frecuencia de los informes puede personalizarse según tus preferencias, con opciones para actualizaciones semanales, quincenales o mensuales.",
+  },
+  {
+    question: "¿Ofrecen soporte continuo si tengo preguntas o necesito ajustes en mi estrategia?",
+    answer:
+      "Por supuesto. Ofrecemos soporte continuo para atender cualquier pregunta o inquietud que puedas tener. Nuestro equipo está disponible para asistirte y realizar ajustes en tu estrategia según sea necesario para que se alinee con los objetivos en evolución de tu negocio.",
+  },
+  {
+    question: "¿Puedo ver una demo o recibir una consulta antes de decidir trabajar con su agencia?",
+    answer:
+      "¡Claro que sí! Ofrecemos consultas gratuitas y demos personalizadas para mostrar nuestro enfoque, discutir tus necesidades y determinar cómo podemos apoyar mejor a tu negocio. Contáctanos para agendar una consulta.",
+  
+  },
+];
+
+export { services, solutions, faqs };
